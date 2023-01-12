5de2f8e4adc348ecdd97bf7635993881c353d901
Raffaele Pansini <r_pansini@hotmail.com>
2023-01-12 17:03:25 +0100

    Update to build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Layout from './components/layout'
-import Header from './components/header'
-import Board from './components/board'
+import Layout from './../components/layout'
+import Header from './../components/header'
+import Board from './../components/board'
 
 
 export default function Home() {
