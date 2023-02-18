503fc4ee1121110e418a5f8c0065ebe5d36daeac
rpansini <r_pansini@hotmail.com>
2023-02-19 00:48:58 +0100

    Update board

--- components/board.js
+++ components/board.js
@@ -4,142 +4,114 @@ import { v4 as uuidv4 } from "uuid";
 import NewCardModal from "./newCardModal";
 //import { todoList, inProgressList, doneList } from "./data"
 import { useEffect, useState } from "react";
-import Image from "next/image"; 
+import Image from "next/image";
 
 function Board() {
-  const [cards, setCards] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [dragged, setDragged] = useState(null);
-  const [listOfList, setListOfList] = useState({
-    todoList: [],
-    inProgressList: [],
-    doneList: [],
-  });
-
-  function handleDrop(event) {
-    const list = event.currentTarget.dataset.id;
-    const listOfListClone = structuredClone(listOfList);
-
-    // Aca tendra que ir el codigo para poder hacer el drop de las nuevas cards
-
-    const newList = listOfListClone[dragged.list].filter(
-      (item) => item.id !== dragged.data.id
-    );
-    listOfListClone[dragged.list] = newList;
-    listOfListClone[list].push(dragged.data);
-    setListOfList(listOfListClone);
-  }
-
-  const loadData = async () => {
-    const doneResponse = await fetch("/api/done");
-    const doneJson = await doneResponse.json();
-
-    const inProgressResponse = await fetch("/api/inProgress");
-    const inProgressJSON = await inProgressResponse.json();
-
-    const todoResponse = await fetch("/api/todo");
-    const todoJSON = await todoResponse.json();
-
-    setListOfList({
-      todoList: todoJSON,
-      inProgressList: inProgressJSON,
-      doneList: doneJson,
+    const [cards, setCards] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [dragged, setDragged] = useState(null);
+    const [listOfList, setListOfList] = useState({
+        todoList: [],
+        inProgressList: [],
+        doneList: [],
     });
-  };
 
-  const addCardTodo = (card) => {
-    const list = { ...listOfList };
-    list.todoList.push(card);
-    setListOfList(list);
-  };
+    function handleDrop(event) {
+        const list = event.currentTarget.dataset.id;
+        const listOfListClone = structuredClone(listOfList);
 
-  const addCardInProgress = (card) => {
-    const list = { ...listOfList };
-    list.inProgressList.push(card);
-    setListOfList(list);
-  };
+        // Aca tendra que ir el codigo para poder hacer el drop de las nuevas cards
 
-  const addCardDone = (card) => {
-    const list = { ...listOfList };
-    list.doneList.push(card);
-    setListOfList(list);
-  };
+        const newList = listOfListClone[dragged.list].filter(
+            (item) => item.id !== dragged.data.id
+        );
+        listOfListClone[dragged.list] = newList;
+        listOfListClone[list].push(dragged.data);
+        setListOfList(listOfListClone);
+    }
 
-  const handleAddCardClick = () => {
-    setIsModalOpen(true);
-  };
+    const loadData = async () => {
+        const doneResponse = await fetch("/api/done");
+        const doneJson = await doneResponse.json();
 
-  const handleCreateCard = (title) => {
-    const newCard = {
-      key: uuidv4(),
-      title: title,
+        const inProgressResponse = await fetch("/api/inProgress");
+        const inProgressJSON = await inProgressResponse.json();
+
+        const todoResponse = await fetch("/api/todo");
+        const todoJSON = await todoResponse.json();
+
+        setListOfList({
+            todoList: todoJSON,
+            inProgressList: inProgressJSON,
+            doneList: doneJson,
+        });
     };
-    setCards([...cards, newCard]);
-    setIsModalOpen(false);
-    console.log(newCard);
-  };
 
-  useEffect(() => {
-    loadData();
-  }, []);
+    const handleAddCardClick = () => {
+        setIsModalOpen(true);
+    };
 
-  return (
-    <div className="flex flex-col flex-1 gap-4 p-4">
-      <div>
-        <h1 className="text-2xl font-bold">Development</h1>
-      </div>
-      <main className="flex flex-1 gap-6" >
-        <List
-          title="TODO"
-          openModal={handleAddCardClick}
-          modalBoton={"Add new Card"}
-          signoPlus= {<Image src= "/plus.svg" width= '20' height= '20' alt= 'Agregar card' title='Agregar nueva card'/>}
-          handleDrop={handleDrop}
-          handleCreateCard={addCardTodo}
-          id="todoList"
-        >
-          {listOfList.todoList.map((item) => (
-            <Card {...item} key={item.id} setDragged={setDragged} />
-          ))}
-          
-          {isModalOpen && (
-            <NewCardModal
-              setIsModalOpen={setIsModalOpen}
-              handleCreateCard={handleCreateCard}
-            />
-          )}
-          {cards.map((card) => (
-            <Card
-              {...card}
-              setDragged={setDragged}
-              title={card.title}
-              key={card.key}
-            />
-          ))}
-        </List>
-        <List
-          title="In Progress"
-          handleDrop={handleDrop}
-          handleCreateCard={addCardInProgress}
-          id="inProgressList"
-        >
-          {listOfList.inProgressList.map((item) => (
-            <Card {...item} key={item.id} setDragged={setDragged} />
-          ))}
-        </List>
-        <List
-          title="Done"
-          handleDrop={handleDrop}
-          handleCreateCard={addCardDone}
-          id="doneList"
-        >
-          {listOfList.doneList.map((item) => (
-            <Card {...item} key={item.id} setDragged={setDragged} />
-          ))}
-        </List>
-      </main>
-    </div>
-  );
+    const handleCreateCard = (title) => {
+        const newCard = {
+            key: uuidv4(),
+            title: title,
+        };
+        const newList = { ...listOfList };
+        newList.todoList.push(newCard);
+        setListOfList(newList);
+        setIsModalOpen(false);
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    return (
+        <div className="flex flex-col flex-1 gap-4 p-4">
+            <div>
+                <h1 className="text-2xl font-bold">Development</h1>
+            </div>
+            <main className="flex flex-1 gap-6" >
+                <List
+                    title="TODO"
+                    openModal={handleAddCardClick}
+                    modalBoton={"Add new Card"}
+                    signoPlus={<Image src="/plus.svg" width='20' height='20' alt='Agregar card' title='Agregar nueva card' />}
+                    handleDrop={handleDrop}                    
+                    id="todoList"
+                >
+                    {listOfList.todoList.map((item) => (
+                        <Card {...item} key={item.id} setDragged={setDragged} />
+                    ))}
+
+                    {isModalOpen && (
+                        <NewCardModal
+                            setIsModalOpen={setIsModalOpen}
+                            handleCreateCard={handleCreateCard}
+                        />
+                    )}                   
+                </List>
+                <List
+                    title="In Progress"
+                    handleDrop={handleDrop}                    
+                    id="inProgressList"
+                >
+                    {listOfList.inProgressList.map((item) => (
+                        <Card {...item} key={item.id} setDragged={setDragged} />
+                    ))}
+                </List>
+                <List
+                    title="Done"
+                    handleDrop={handleDrop}                    
+                    id="doneList"
+                >
+                    {listOfList.doneList.map((item) => (
+                        <Card {...item} key={item.id} setDragged={setDragged} />
+                    ))}
+                </List>
+            </main>
+        </div>
+    );
 }
 
 export default Board;
